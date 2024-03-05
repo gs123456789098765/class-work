8fe1be7b0ca2c15ced092709310217db20d815e1
GeorgeSpoth <157054378+gs123456789098765@users.noreply.github.com>
2024-03-05 15:06:53 -0500

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,19 @@
+na = input("name")
+
+jo = input("job")
+
+ga = input("game")
+
+print("Once upon a time there was a man named")
+
+print(na)
+
+print("who worked as")
+
+print(jo)
+
+print("engineer in a bustling city")
+
+print("After long days at the office Jack found solace in playing")
+
+print(ga)
